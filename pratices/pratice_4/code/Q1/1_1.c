54f6127292f2c1fe070c5a66c832083de7f9a446
ELTON MAURICIO DA SILVA <elton.silva@luizalabs.com>
2023-04-02 20:22:57 -0300

    pratice 4

---
+++ pratices/pratice_4/code/Q1/1_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define Size 64
+
+int main() {
+    int A[Size][Size], B[Size][Size], C[Size][Size];
+    int i, j;
+    int page_faults = -3; // variável para contagem dos page_faults
+    for (j = 0; j< Size; j ++) {
+        for (i = 0; i< Size; i++) {
+            C[i][j] = A[i][j] + B[i][j];
+            if (i % 3 == 0) page_faults += 3; // A cada 3 iterações, acontecem 3 page_faults
+        }
+    }
+
+    printf("page_faults: %d\n", page_faults);
+
+    return 0;
+}
